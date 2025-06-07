cde386a2b04a0537f82ba7c04a78966edd68a58d
theg <52027622+theg1239@users.noreply.github.com>
2025-06-07 18:55:55 +0530

    fix: update memory and duration settings for API functions in Vercel configuration

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+    experimental: {
+    serverComponentsExternalPackages: ["playwright-core", "@sparticuz/chromium"],
+  },
 }
 
 export default nextConfig

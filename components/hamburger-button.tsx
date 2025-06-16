6e05aa4d29d11e31d27bfe98ea4d6cd6c34a0a09
theg <52027622+theg1239@users.noreply.github.com>
2025-06-16 15:35:30 +0530

    update HamburgerButtonProps to use React.MouseEventHandler for onClick type

--- components/hamburger-button.tsx
+++ components/hamburger-button.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { Button } from '@/components/ui/button'
 
 interface HamburgerButtonProps {
-  onClick: () => void
+  onClick: React.MouseEventHandler<HTMLButtonElement>
   className?: string
 }
 

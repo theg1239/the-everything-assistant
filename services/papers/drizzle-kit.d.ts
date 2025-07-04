09b20b77e3bafc51cae682b9d8cbafa8e95c8af6
theg <52027622+theg1239@users.noreply.github.com>
2025-07-04 14:05:03 +0530

    feat: remove type declarations for drizzle-kit and drizzle-orm modules

--- services/papers/drizzle-kit.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'drizzle-kit' {
-  const defineConfig: (...args: any[]) => any;
-  export { defineConfig };
-}
-
-declare module 'drizzle-orm' {
-  const defineConfig: (...args: any[]) => any;
-  export { defineConfig };
-}

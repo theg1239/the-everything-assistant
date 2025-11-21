5a44ee7c172f8ea8b3a035f9e0c1d29743a9f383
theg <thegreatestdropper@gmail.com>
2025-11-21 20:58:45 +0530

    fix: update import path for prisma in auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,8 @@ import { NextAuthOptions } from 'next-auth'
 import { getServerSession } from 'next-auth/next'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { prisma } from './prisma'
+import { prisma } from '@/lib/prisma'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
